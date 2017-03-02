1e52223e780f8562682dbab1396661257208c9ec
Andrew Herr <lobsterless@gmail.com>
2017-03-02 10:28:19 -0700

    add nxml mode configuration and web mode config

---
+++ personal/26-nxml-mode.el
@@ -0,0 +1,5 @@
+;; Set indentation width to 2 spaces in nxml mode
+(setq
+ nxml-child-indent 2
+ nxml-attribute-indent 2
+ nxml-slash-auto-complete-flag t)
---
+++ personal/preload/24-web-mode.el
@@ -0,0 +1,2 @@
+;;; web-mode.org says this should be set before web-mode is loaded
+(setq web-mode-enable-current-column-highlight t)
