1e14998e4b271fd2ac66fcd65469498ca1d3afb4
Andrew Herr <andrew.herr@itron.com>
2018-02-10 15:47:23 -0700

    Add server start on mac

--- personal/12-mac-itron.el
+++ personal/12-mac-itron.el
@@ -6,7 +6,7 @@
 
   (setq prelude-clean-whitespace-on-save nil) ;; don't clean up code here
   (global-aggressive-indent-mode 0) ;; off at Itron so I don't reformat all the things
-
+  
   ;; all the lines are too long, switch to fill column indicator
   (setq whitespace-style '(face tabs empty trailing))
   ;; (setq fci-rule-width 1)
@@ -14,4 +14,5 @@
   ;; (define-globalized-minor-mode global-fci-mode fci-mode (lambda () (fci-mode 1)))
   ;; (global-fci-mode 1)
 
+  (server-start)
   )
